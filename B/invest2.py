3d867926c64f6436a3526a864e01dcb3ea8e2140
rule2003 <rllamas22@jaumebalmes.net>
2023-01-16 20:16:04 +0100

    practica Raul

---
+++ B/invest2.py
@@ -0,0 +1,20 @@
+
+#Imprimir el total dels diners acumulats
+#després de 7 anys, utilitzant variables (comentari a afegir al codi)
+
+
+savings = 100
+increase = 1.1
+result = savings
+i = 0
+while i < 7:
+    print(result)
+    result*=increase
+    i+=1
+
+
+
+
+
+
+
